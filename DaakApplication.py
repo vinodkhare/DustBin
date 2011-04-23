8ee913e91d44a4631e302de8ec96e0de8cce70f3
Vinod Khare <vinod.khare@gmail.com>
2011-04-23 17:05:37 -0400

    Importing classes from Daak. Will use as templates.

---
+++ DaakApplication.py
@@ -0,0 +1,44 @@
+import gtk
+import gobject
+
+from DaakIndicator import *
+from DaakPrefsWindow import *
+from pygmail import *
+from keyring import *
+
+class DaakApplication:
+	def __init__(self):
+		self.indicator = DaakIndicator("daak", "/home/vinod/Code/Daak/daak-new.png", appindicator.CATEGORY_APPLICATION_STATUS)
+		self.indicator.initWidgets()
+	
+		self.gmailClient = pygmail()
+		self.gmailClient.login('vinod.khare', 'Ncc1737d')
+		nUnread = self.gmailClient.get_unread_count()
+		self.indicator.set_label(str(nUnread))
+	
+		self.prefsWindow = DaakPrefsWindow()
+		self.prefsWindow.initWidgets()
+    
+		# Make connections
+		#~ self.indicator.prefsMenuItem.connect('activate', self.prefsWindow.toggleVisible)
+		self.indicator.prefsMenuItem.connect('activate', self.prefsWindow.presentWindow)
+		self.indicator.checkMenuItem.connect('activate', self.checkNow)
+		#~ self.indicator.timeMenuItem.connect('activate', self.timeNow)
+		
+		#add the timer
+		gobject.timeout_add(5000, self.checkNow)
+	
+	def checkNow(self, widget = None):
+		#~ print "checkNow()"
+		nUnread = self.gmailClient.get_unread_count()
+		self.indicator.set_label(str(nUnread))
+		return True
+		
+	#~ def timeNow(self, widget):
+		#~ gobject.timeout_add(5000, self.checkNow)
+	
+	def main(self):
+		daakKeyring = Keyring('daak', 'gmail.com', 'imap')
+		print daakKeyring.has_credentials()
+	
+		gtk.main()
